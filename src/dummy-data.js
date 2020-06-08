1f32ec04570679bd127b6b307721a31ee90d49be
Samuel Earl <samueltremain@gmail.com>
2020-06-08 12:23:09 -0700

    Added some notes and changed the timestamp format

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -1,99 +1,110 @@
+/**
+ * Once a user is logged in, they should see all posts from all users on their dashboard. You could
+ * implement pagination to show only a certain number of posts at a time, but I won't implement
+ * pagination in this app.
+ *
+ * Dgraph has a dateTime scalar type that uses RFC 3339 format
+ * (https://dgraph.io/docs/query-language/#schema-types), so we need to convert our timestamps for
+ * the createdAt fields into RFC 3339 format before sending those timestamps to Dgraph. This is how
+ * you convert a JavaScript date object into RFC 3339 format:
+ * https://codetogo.io/how-to-create-date-in-rfc3339-format-in-javascript/.
+ */
 // export const postsArray = [];
 export const postsArray = [
   {
     id: "1",
     firstName: "John",
     lastName: "Doe",
-    createdAt: Date.now(),
+    createdAt: (new Date()).toISOString(),
     content: "Content string...",
+    likes: 2,
     comments: [
       {
         id: "1",
         firstName: "James",
         lastName: "Howard",
-        createdAt: Date.now(),
+        createdAt: (new Date()).toISOString(),
         content: "This is James' comment...",
       },
       {
         id: "2",
         firstName: "Bill",
         lastName: "Smith",
-        createdAt: Date.now(),
+        createdAt: (new Date()).toISOString(),
         content: "This is Bill's comment...",
       },
       {
         id: "3",
         firstName: "Mary",
         lastName: "Taylor",
-        createdAt: Date.now(),
+        createdAt: (new Date()).toISOString(),
         content: "This is Mary's comment...",
       },
       {
         id: "4",
         firstName: "Betty",
         lastName: "Johnson",
-        createdAt: Date.now(),
+        createdAt: (new Date()).toISOString(),
         content: "This is Betty's comment...",
       },
       {
         id: "5",
         firstName: "David",
         lastName: "Garrison",
-        createdAt: Date.now(),
+        createdAt: (new Date()).toISOString(),
         content: "This is David's comment...",
       }
-    ],
-    likes: 2
+    ]
   },
   {
     id: "2",
     firstName: "John",
     lastName: "Doe",
-    createdAt: Date.now(),
+    createdAt: (new Date()).toISOString(),
     content: "Another content string...",
+    likes: 1,
     comments: [
       {
         id: "2",
         firstName: "Jane",
         lastName: "Doe",
-        createdAt: Date.now(),
+        createdAt: (new Date()).toISOString(),
         content: "Another comment goes here...",
       }
-    ],
-    likes: 1
+    ]
   },
   {
     id: "3",
     firstName: "John",
     lastName: "Doe",
-    createdAt: Date.now(),
+    createdAt: (new Date()).toISOString(),
     content: "Content string...",
+    likes: 1,
     comments: [
       {
         id: "3",
         firstName: "James",
         lastName: "Howard",
-        createdAt: Date.now(),
+        createdAt: (new Date()).toISOString(),
         content: "Comment goes here...",
       }
-    ],
-    likes: 1
+    ]
   },
   {
     id: "4",
     firstName: "John",
     lastName: "Doe",
-    createdAt: Date.now(),
+    createdAt: (new Date()).toISOString(),
     content: "Content string...",
+    likes: 1,
     comments: [
       {
         id: "4",
         firstName: "James",
         lastName: "Howard",
-        createdAt: Date.now(),
+        createdAt: (new Date()).toISOString(),
         content: "Comment goes here...",
       }
-    ],
-    likes: 1
+    ]
   }
 ];
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,6 @@
 import moment from "moment";
 
 export const formatDate = (timestamp) => {
-  console.log("timestamp:", timestamp);
   if (!timestamp) return '-';
   return moment(timestamp).fromNow();
 };
